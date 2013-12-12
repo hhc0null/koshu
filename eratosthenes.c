c651ec11df9ec7654cc05c5908ccd9352d6c2a55
hiroki.matsukuma <hhc.0.null@gmail.com>
2013-12-12 17:57:46 +0900

    アーッアーッ

--- eratosthenes.c
+++ eratosthenes.c
@@ -9,27 +9,23 @@ int main(void)
     bool *is_prime = NULL;
 
     scanf("%Ld", &max);
+    max++;
     is_prime = (bool *)calloc(max, sizeof(bool));
     if(is_prime == NULL) {
         puts("cannot allocate memory.");
         exit(EXIT_FAILURE);
     }
+
     memset(is_prime+2, true, max);
-
-    for(int i = 2; i < max; i++) {
-        if(*(is_prime+i) == true) {
-            for(int j = i*2; j < max; j+=i) {
+    for(int i = 2; i < max; i++)
+        if(*(is_prime+i) == true)
+            for(int j = i*2; j < max; j+=i)
                 *(is_prime+j) = false;
-            }
-        }
-    }
 
-    for(int i = 0; i < max; i++) {
+    for(int i = 0; i < max; i++)
         if(*(is_prime+i) == true)
             printf("%d\n", i);
-    }
 
     free(is_prime);
-
     return 0;
 }
